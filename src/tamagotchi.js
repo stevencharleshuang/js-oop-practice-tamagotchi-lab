7e2b34a7372760c3753d038dd840835c61f1c203
Avi Lichtschein <AviLichtschein@gmail.com>
2017-12-05 10:35:27 -0500

    Create tamagotchi.js

---
+++ src/tamagotchi.js
@@ -0,0 +1,62 @@
+/**
+	 * Object literal declarations for each: Cat, Dog, Fish.
+	 * Each should have the following properties:
+	 * @name {hungry} - initially set to false
+	 * @name {sick} - initially set to false
+	 * @name {age} - intially set to 0
+	 * name {restLevel} - initially set to 5
+	 * should have the folloving methods:
+	 * @name {feed}
+	 * @param {nothing}
+	 * @desc
+	 		- if hungry is true, return "That was yummy!" and set hungry to false. If hungry is false, return "No thanks, I'm full." and set hungry to true.
+	 * @return {nothing}
+	 * @name {sleep}
+	 * @desc
+	   	- if hungry is true, return "That was yummy!" and set hungry to false. If hungry is false, return "No thanks, I'm full." and set hungry to true.
+	 * @returns {string}
+	 * @name {medicate}
+	 * @desc
+	    - if sick is true, return "I feel much better!" and set sick to false. If sick is false, return "No thanks, I feel fine." and set sick to true.
+		* @returns {string}
+		* @name {increaseAge}
+		* @desc
+			- increase the age by one and return "Happy Birthday to me! I am " + age + " years old!"
+		* @returns {string}
+	*/
+
+
+
+	/*  Dog specific abilities:
+		* @name {bark}
+		* @returns {string} - "ruff ruff"
+		* @name  {goOutside}
+    * @returns "I own that tree now!"
+		Cat specific abilities:
+		* @name {meow}
+		* @returns {string} - "Meow!"
+		* @name {useLitterBox}
+		* @returns {string} - "Burried treasure!"
+		Fish specific abilities:
+		* @name {swim}
+		* @returns {string} - "Just keep swimming..."
+		*/
+
+
+/* Feed the dog twice */
+
+/* Increase the dog's age twice */
+
+/* Make the dog bark */
+
+/* Make the dog go outside */
+
+/* Make the cat meow */
+
+/* Make the cat sleep */
+
+/* Make the cat use the litter box */
+
+/* Give the fish medicine twice */
+
+/* Make the fish swim */
